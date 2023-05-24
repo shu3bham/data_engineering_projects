afec6f07ea0133142a6a0f4fbcdf297b6faeacc8
shu3bham <shubhamdeshmukhnagpur@gmail.com>
2023-05-24 22:01:33 +0530

    added dag file and make etl function

--- twitter_etl.py
+++ twitter_etl.py
@@ -6,40 +6,39 @@ import s3fs
 import re
 
 
-#function to remove url
+
+def run_twitter_etl():
+
+    #twitter authetication
+    Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAJAYigEAAAAAN1Fuk%2B2EsNSaqcrBz3UGGP8j%2BFs%3DGVczWyYOuBEQH0JldmeCzxXMh4jvusPmhJTUZj24YOMAD9EZGE'
+    auth = tweepy.OAuth2BearerHandler(Bearer_Token)
+
+    #creating an api object
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name='@FoxNews',count = 2000, include_rts = False, tweet_mode = 'extended')
+    tweet_list=[]
 
 
 
-#twitter authetication
-Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAJAYigEAAAAAN1Fuk%2B2EsNSaqcrBz3UGGP8j%2BFs%3DGVczWyYOuBEQH0JldmeCzxXMh4jvusPmhJTUZj24YOMAD9EZGE'
-auth = tweepy.OAuth2BearerHandler(Bearer_Token)
+    for tweet in tweets:
+        text =tweet._json['full_text']
 
-#creating an api object
-api = tweepy.API(auth)
-tweets = api.user_timeline(screen_name='@FoxNews',count = 2000, include_rts = False, tweet_mode = 'extended')
-tweet_list=[]
+        refined_tweet = {
+            "user":tweet.user.screen_name,
+            "text": text,
+            "favorite_count" : tweet.favorite_count,
+            "retweet_count" : tweet.retweet_count,
+            "created_at" : tweet.created_at
 
+        }
 
-
-for tweet in tweets:
-    text =tweet._json['full_text']
-
-    refined_tweet = {
-        "user":tweet.user.screen_name,
-        "text": text,
-        "favorite_count" : tweet.favorite_count,
-        "retweet_count" : tweet.retweet_count,
-        "created_at" : tweet.created_at
-
-    }
-
-    tweet_list.append(refined_tweet)
+        tweet_list.append(refined_tweet)
 
 
 
-#creating dataFrame
-df = pd.DataFrame(tweet_list)
-df.to_csv("fox_news_tweet_data.csv")
+    #creating dataFrame
+    df = pd.DataFrame(tweet_list)
+    df.to_csv("s3://projectbuckee/twitter_data/fox_news_tweet_data.csv")
 
 
 
